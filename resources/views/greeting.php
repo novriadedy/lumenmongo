048de703759a25b1343e0f8e4fabfa4154cf0403
novriadedy <novriadedy@gmail.com>
2019-08-10 05:29:56 +0700

    edit javascript checklist

--- resources/views/greeting.php
+++ resources/views/greeting.php
@@ -244,19 +244,6 @@ $(document).ready(function(){
 			}
 		 });
 	});
-
-$("#mytable #checkall").click(function () {
-        if ($("#mytable #checkall").is(':checked')) {
-            $("#mytable input[type=checkbox]").each(function () {
-                $(this).prop("checked", true);
-            });
-
-        } else {
-            $("#mytable input[type=checkbox]").each(function () {
-                $(this).prop("checked", false);
-            });
-        }
-    });
     
     $("[data-toggle=tooltip]").tooltip();
 });
